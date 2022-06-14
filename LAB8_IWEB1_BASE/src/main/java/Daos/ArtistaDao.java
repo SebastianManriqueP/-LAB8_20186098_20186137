eadc62205b0c9e2d45d7cf002eceb30ebb5a3e2b
raykevinconcha <tudela2352>
2022-06-13 20:50:08 -0500

    4

---
+++ LAB8_IWEB1_BASE/src/main/java/Daos/ArtistaDao.java
@@ -0,0 +1,21 @@
+package Daos;
+
+import Beans.Artista;
+import java.util.ArrayList;
+import java.sql.*;
+
+public class ArtistaDao {
+    private static String user = "root";
+    private static String pass = "root";
+    private static String url = "jdbc:mysql://localhost:3306/lab6sw1";
+
+    public ArrayList<Artista> obtenerartistas() {
+        ArrayList<Cancion> listaArtistas = new ArrayList<>();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+}
